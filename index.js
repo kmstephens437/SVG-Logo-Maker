4ed65643a66ac717eb09389fdb90bc8bf49fe886
Kevin-Stephens <kmstephens437@gmail.com>
2023-08-30 12:50:47 -0500

    shape rendering modules

--- index.js
+++ index.js
@@ -6,16 +6,31 @@ const questions = () => inquirer.prompt ([
         name: 'text',
         type: 'input',
         message:'Please enter 3 letters for your logo.'
-    }
+    },
+    {
+        name: 'textColor',
+        type: 'input',
+        message:'Please enter a color for your text.'
+    },
     {
         name: 'shape',
         type: 'list',
         message: 'Please select a shape for your logo.' ,
         choices: ['Circle', 'Square','Triangle']
+    },
+    {
+        name: 'shapeColor',
+        type: 'input',
+        message: 'Please enter a color for your shape.'
     }
+
+
 ])
 
 questions()
-.then((answers) => {
-    
-})
+    .then((answers) => {
+        const generated = generateSvg(answers);
+        fs.writeFileSync ('logo.svg', generated)
+    })
+    .then(() => console.log('Success! Logo created'))
+    .catch(err => console.log(err));
