5f7d90a5600947fea7de8e6108dd3aa50925f3a6
Kevin-Stephens <kmstephens437@gmail.com>
2023-08-29 19:29:59 -0500

    Classes

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const inquirer = require ("inquirer");
-const fs = require ("fs");
+const fs = require ("fs")
+
+const questions = () => inquirer.prompt ([
+    {
+        name: 'text',
+        type: 'input',
+        message:'Please enter 3 letters for your logo.'
+    }
+    {
+        name: 'shape',
+        type: 'list',
+        message: 'Please select a shape for your logo.' ,
+        choices: ['Circle', 'Square','Triangle']
+    }
+])
+
+questions()
+.then((answers) => {
+    
+})
